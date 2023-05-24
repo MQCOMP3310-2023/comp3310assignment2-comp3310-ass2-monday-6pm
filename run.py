683dda36112e45a5a7aba9b320ff0720f93934ad
kholgate <keaganholgate@outlook.com>
2023-05-24 14:48:55 +1000

    Resolve CWE-215, CWE489 'Flask app is run in debug mode'

--- run.py
+++ run.py
@@ -2,6 +2,8 @@ from project import create_app
 
 if __name__ == '__main__':
   app = create_app()
-  app.run(host = '0.0.0.0', port = 8000, debug=True)
+  app.run(host = '0.0.0.0', port = 8000)
 
 # Remove debug=True line. Insecure. 
+# A Flask app appears to be run in debug mode. This may allow an attacker to run arbitrary code through the debugger. 
+# Running a Flask application with debug mode enabled may allow an attacker to gain access through the Werkzeug debugger.
