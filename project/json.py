3b5b95808f3031dcd01041baa4b6f4b84f21026e
kholgate <keaganholgate@outlook.com>
2023-05-31 21:26:21 +1000

    Resolve security issue: "SQL query built from user-controlled sources" Two occurrances resolved in json.py

--- project/json.py
+++ project/json.py
@@ -9,14 +9,22 @@ json = Blueprint('json', __name__)
 #JSON APIs to view Restaurant Information
 @json.route('/restaurant/<restaurant_id>/menu/JSON')
 def restaurantMenuJSON(restaurant_id):
-    items = db.session.execute(text('select * from menu_item where restaurant_id = ' + str(restaurant_id)))
+#   Original code, vulnerable to injection
+    #   items = db.session.execute(text('select * from menu_item where restaurant_id = ' + str(restaurant_id)))
+#   Parameterised queries with SQLAlchemy:
+    query = text('select * from menu_item where restaurant_id = :restaurant_id LIMIT 1')
+    result = db.session.execute(query, {'restaurant_id' : restaurant_id})
     items_list = [ i._asdict() for i in items ]
     return pyjs.dumps(items_list)
 
 
 @json.route('/restaurant/<restaurant_id>/menu/<int:menu_id>/JSON')
 def menuItemJSON(restaurant_id, menu_id):
-    Menu_Item = db.session.execute(text('select * from menu_item where id = ' + str(menu_id) + ' limit 1'))
+#   Original code, vulnerable to injection
+    #   Menu_Item = db.session.execute(text('select * from menu_item where id = ' + str(menu_id) + ' limit 1'))
+#   Parameterised queries with SQLAlchemy:
+    query = text('select * ffrom menu_item WHERE id = :menu_id LIMIT 1')
+    result = db.session.execute(query, {'menu_id': menu_id})
     items_list = [ i._asdict() for i in Menu_Item ]
     return pyjs.dumps(items_list)
 
